5c018bb6d65be507735bbbab8376e84037b9dc09
Krzesimir Samborski <chris710@o2.pl>
2014-10-19 17:57:06 +0200

    WORKING
    - fixed volatile struct initiation in my_fopen
    - added exception handling in my_fread and my_fwrite

--- IOLib/my_stdio.c
+++ IOLib/my_stdio.c
@@ -15,13 +15,25 @@ MY_FILE *my_fopen(char *name, char *mode) {
 
 	/* MY_FILE structure initialization */
 	char* new_buffer = (char*)malloc(sizeof(char)*BUFFER_SIZE);
-	MY_FILE new_file = { new_desc, new_buffer, new_buffer, 0, 0, 0 };
+	char* new_wbuffer = (char*)malloc(sizeof(char)*BUFFER_SIZE);
+	//static MY_FILE new_file;
+	MY_FILE* new_file = (MY_FILE*)malloc(sizeof(MY_FILE*));
+	//new_file = { new_desc, new_buffer, new_wbuffer, 0, 0, 0 };
+	new_file->file = new_desc;
+	new_file->rbuffer = new_buffer;
+	new_file->wbuffer = new_wbuffer;
+	new_file->pointer = 0;
+	new_file->wpointer = 0;
+	new_file->eof = 0;
 
-	return &new_file;
+	return new_file;
+	//return &new_file;
 }
 
 int my_fclose(MY_FILE *f) {
 	/* free all the resources of MY_FILE */
+	if (close(f->file) < 0)
+		return 0;
 	free(f->wbuffer);
 	free(f->rbuffer);
 	if (f->wbuffer == NULL && f->rbuffer == NULL)
@@ -39,7 +51,8 @@ int my_fread(void *p, size_t size, size_t nbelem, MY_FILE *f) {
 	
 	if (nbelem > BUFFER_SIZE) {		//check if the size is bigger than the buffer
 		int rest = (nbelem - (BUFFER_SIZE - f->pointer));	//how much more do need to read
-		eof = read(f->file, f->rbuffer,(BUFFER_SIZE - f->pointer));		//read what is left in the buffer
+		if (eof = read(f->file, f->rbuffer, (BUFFER_SIZE - f->pointer)) < 0)		//read what is left in the buffer
+			return -1;
 		if (!eof) {		//no bytes have been read
 			f->eof == 0;
 			return 0;
@@ -47,7 +60,8 @@ int my_fread(void *p, size_t size, size_t nbelem, MY_FILE *f) {
 		memcpy(p, f->rbuffer[f->pointer], eof);
 		//for (i = 0; i < times; ++i) {
 		while (rest>=0) {
-			eof = read(f->file, f->rbuffer,(rest>BUFFER_SIZE ? BUFFER_SIZE : rest));
+			if( eof = read(f->file, f->rbuffer,(rest>BUFFER_SIZE ? BUFFER_SIZE : rest)) < 0)
+				return -1;
 			memcpy(p, f->rbuffer[0], eof);
 			rest -= BUFFER_SIZE;
 			/* check if eof */
@@ -59,7 +73,8 @@ int my_fread(void *p, size_t size, size_t nbelem, MY_FILE *f) {
 	}
 		// check if we need to read at all
 	else if (f->pointer == 0 || (f->pointer + nbelem) > BUFFER_SIZE) {		//no data has been read yet or not all data is not in buffer
-		eof = read(f->file, f->rbuffer,BUFFER_SIZE);	//read as much as you can
+		if (eof = read(f->file, f->rbuffer, BUFFER_SIZE) < 0)	//read as much as you can
+			return -1;
 		/* check if eof */
 		if (!eof) {
 			f->eof = 1;
@@ -85,13 +100,15 @@ int my_fwrite(void *p, size_t taille, size_t nbelem, MY_FILE *f) {
 		int rest = (nbelem - (BUFFER_SIZE - f->wpointer));	//how much more do need to read
 		memcpy(f->wbuffer[0], p, (BUFFER_SIZE - f->wpointer));
 		while (rest>=0) {		//write BUFFER_SIZE chunks till you make it
-			eof = write(f->file, f->wbuffer,(rest>BUFFER_SIZE ? BUFFER_SIZE : rest));
+			if (eof = write(f->file, f->wbuffer, (rest > BUFFER_SIZE ? BUFFER_SIZE : rest)) < 0)
+				return -1;
 			rest -= BUFFER_SIZE;
 		}
 	}
 		// check if we should write already
 	else if (f->wpointer == BUFFER_SIZE-1 || (f->wpointer + nbelem) > BUFFER_SIZE) {		//buffer is full or buffer will overflow
-		eof = write(f->file, f->wbuffer, f->wpointer+1);	//write what you already have
+		if (eof = write(f->file, f->wbuffer, f->wpointer + 1) < 0)	//write what you already have
+			return -1;
 	}
 	else { //just add data to the buffer
 		memcpy(f->wbuffer[f->wpointer],p, nbelem);
